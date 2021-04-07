2650b6e5492371c7540372289cc1a1762e29f34a
CENW10N09260\Resource <fernando.nazareth@qintess.com>
2021-04-07 18:29:59 -0300

    Cria projeto de favorecidos

---
+++ favorecidos-ms/src/main/java/com/fnazareth/ms_favorecidos/FavorecidosMsApplication.java
@@ -0,0 +1,13 @@
+package com.fnazareth.ms_favorecidos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FavorecidosMsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FavorecidosMsApplication.class, args);
+	}
+
+}
---
+++ favorecidos-ms/src/test/java/com/fnazareth/ms_favorecidos/FavorecidosMsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fnazareth.ms_favorecidos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FavorecidosMsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
